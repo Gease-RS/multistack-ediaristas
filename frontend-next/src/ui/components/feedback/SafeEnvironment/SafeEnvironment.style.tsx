59d5cd6ae605b67525e1648b64b47754d46d8404
Gease-RS <geasebrasil@gmail.com>
2021-06-10 23:13:18 -0300

    Header

--- frontend-next/src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
+++ frontend-next/src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -1,8 +1,9 @@
-import { experimentalStyled as styled } from "@material-ui/core"
+import { experimentalStyled as styled } from "@material-ui/core";
 
-export const SafeEnvironmentContainer = styled('div') `
-    color: black;
-    text-align: right;
-    padding: 16px 0;
-    font-size: 12px;
+export const SafeEnvironmentContainer = styled("div")`
+  color: ${({ theme }) => theme.palette.text.secondary};
+  background-color: ${({ theme }) => theme.palette.background.default};
+  text-align: right;
+  padding: ${({ theme }) => theme.spacing(2)} 0;
+  font-size: 12px;
 `;
