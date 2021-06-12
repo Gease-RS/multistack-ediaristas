01b30acdc4edede33045c6e9ca949ceb145530d7
Gease-RS <geasebrasil@gmail.com>
2021-06-11 22:36:13 -0300

    Input Mask cep

--- frontend-next/src/pages/index.tsx
+++ frontend-next/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography } from "@material-ui/core";
+import { FormElementsContainer } from "@styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,6 +15,24 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
+
+      <FormElementsContainer>
+        <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          variant={"outlined"}
+          fullWidth
+        />
+        <Typography color={"error"}>CEP inválido</Typography>
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          sx={{ width: "220px" }}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
+
       <UserInformation
         name={"Akira Hanashiro"}
         picture={"https://github.com/hanashiro.png"}
